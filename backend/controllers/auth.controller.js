e7f60bef6816334c3b29d061eb6bd9e7cfe008fb
T4CEE <cyriliniekong@gmail.com>
2024-10-01 03:43:04 +0100

    project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res, next) => {
+  res.json({
+    message: "Signup successful",
+  });
+};
+export const login = async (req, res, next) => {
+  res.json({
+    message: "login successful",
+  });
+};
+export const logout = async (req, res, next) => {
+  res.json({
+    message: "logout successful",
+  });
+};
